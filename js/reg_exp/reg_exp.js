c8f86554f9955713b3eedf978f04231b3b16eb94
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-06-26 22:05:18 +0300

    Добавляет терминологию и описание символьных наборов с примерами

--- js/reg_exp/reg_exp.js
+++ js/reg_exp/reg_exp.js
@@ -32,6 +32,41 @@
 // console.log(str.match(reg));
 
 // \S
-let str = `Сколько вы сказали стоят апельсины? 200? - Нет, 150 рублей за килограмм.`
-    reg = /\d\d\S/;
+// let str = `А сколько вы сказали стоят апельсины? 200?- Нет, 150 рублей за килограмм.`
+//     reg = /\d\d\S/;
+// console.log(str.match(reg));
+
+// \w
+// let str = `How are you?`,
+//     reg = /\w/;
+// console.log(str.match(reg));
+
+// \W
+// let str = `Привет, как дела?`
+//     reg = /\W/;
+// console.log(str.match(reg));
+
+// наборы [символы вариантов начала]конец  
+// let str = `Привет, меня зовут Маша а этой мой друг Паша`,
+//     reg = /[МП]аша/;
+// console.log(str.match(reg));
+
+// исключающие наборы 
+// let str = `Привет, меня зовут Маша а этой мой друг Паша. А ты Даша, правильно?`,
+//     reg = /[^МП]аша/;
+// console.log(str.match(reg));
+
+//диапазон 
+// let str = `aбвгдеёжз`,
+//     reg = /[б-д]/;
+// console.log(str.match(reg));
+
+// исключающий диапазон
+// let str = `абвгде`
+//     reg = /[^a-г]/;
+// console.log(str.match(reg));
+
+// несколько диапазонов с исключением
+let str = `абвгдежзиклмн`,
+    reg = /[^а-ез-н]/;
 console.log(str.match(reg));

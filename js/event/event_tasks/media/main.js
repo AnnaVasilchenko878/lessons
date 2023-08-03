f3de4369ffb2bedfaa99bbbc3bc9efc48b457d7f
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-08-03 14:58:55 +0300

    Создает аудиоэлемент и выводит общее время

--- js/event/event_tasks/media/main.js
+++ js/event/event_tasks/media/main.js
@@ -1,20 +1,36 @@
 // Написать обработчики событий для работы с мультимедийными элементами, такими как аудио и видео проигрыватели, чтобы использовать определенные функции, когда пользователь взаимодействует с элементами (например, при нажатии на кнопку "воспроизведение").
 
 // аудиопроигрыватель
-// смена кнопок play и pause
-const btnPlay = document.querySelector('.btn-play'),
-      btnPause = document.querySelector('.btn-pause');
 
-btnPlay.addEventListener('click', function(){
-  this.classList.add('hide');
-  btnPause.classList.remove('hide');
-})
+// создание эл-та аудио c использование встроенного класса audio
+window.addEventListener('DOMContentLoaded', function(){
+  const audio = new Audio();
+  audio.src = 'audio/depeche-mode-enjoy-the-silence.mp3';
 
-btnPause.addEventListener('click', function () {
-  this.classList.add('hide');
-  btnPlay.classList.remove('hide');
-})
+  const btnPlay = document.querySelector('.btn-play'),
+    btnPause = document.querySelector('.btn-pause');
 
-// бесконечное проигрывание плэйлиста
-// регулировка громкости
-// плейлист
+  btnPlay.addEventListener('click', function () {
+    this.classList.add('hide');
+    btnPause.classList.remove('hide');
+    audio.play();
+  })
+
+  btnPause.addEventListener('click', function () {
+    this.classList.add('hide');
+    btnPlay.classList.remove('hide');
+    audio.pause();
+  });
+
+  // вывод продолжительности аудио
+  const timeOutput = document.querySelector('.full-time')
+  audio.addEventListener('loadedmetadata', function(){
+    let time = audio.duration/60;
+    timeOutput.textContent = time.toFixed(2);
+    console.log(time);
+  })
+
+  // бесконечное проигрывание плэйлиста
+  // регулировка громкости
+  // плейлист
+});

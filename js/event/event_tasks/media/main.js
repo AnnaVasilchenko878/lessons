9a1116437b721a32ce2cd388554c0f285b9268a9
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-08-04 11:52:59 +0300

    Добаляет расчет и вывод общего времени трека

--- js/event/event_tasks/media/main.js
+++ js/event/event_tasks/media/main.js
@@ -2,32 +2,46 @@
 
 // аудиопроигрыватель
 
-// создание эл-та аудио c использование встроенного класса audio
+// ждем когда отрисутся DOM
 window.addEventListener('DOMContentLoaded', function(){
+  // создвние экземпляра класса audio
   const audio = new Audio();
   audio.src = 'audio/depeche-mode-enjoy-the-silence.mp3';
 
-  const btnPlay = document.querySelector('.btn-play'),
-    btnPause = document.querySelector('.btn-pause');
-
-  btnPlay.addEventListener('click', function () {
-    this.classList.add('hide');
-    btnPause.classList.remove('hide');
-    audio.play();
-  })
-
-  btnPause.addEventListener('click', function () {
-    this.classList.add('hide');
-    btnPlay.classList.remove('hide');
-    audio.pause();
-  });
-
-  // вывод продолжительности аудио
-  const timeOutput = document.querySelector('.full-time')
+  // ждем когда прогрузиться аудио
   audio.addEventListener('loadedmetadata', function(){
+
+      // получене кнопок и программирование их
+      const btnPlay = document.querySelector('.btn-play'),
+        btnPause = document.querySelector('.btn-pause');
+
+      btnPlay.addEventListener('click', function () {
+        this.classList.add('hide');
+        btnPause.classList.remove('hide');
+        audio.play();
+      })
+
+      btnPause.addEventListener('click', function () {
+        this.classList.add('hide');
+        btnPlay.classList.remove('hide');
+        audio.pause();
+      });
+    
+    // расчет общего времени и вывод 
+    const timeOutput = document.querySelector('.full-time')
     let time = audio.duration/60;
-    timeOutput.textContent = time.toFixed(2);
-    console.log(time);
+    // вывод времени в формате мин:сек 4.23
+    function modFormatTime(currentTime){
+      currentTime = currentTime.toFixed(2);
+      let min = Math.floor(currentTime);
+      let sec = Math.floor((currentTime - min)*100);  
+      return {min: min,
+              sec: sec}    
+    }
+    let fullTime = modFormatTime(time);
+    timeOutput.textContent = `${fullTime.min}:${fullTime.sec}`;
+
+    // timeOutput.textContent = time.toFixed(2);
   })
 
   // бесконечное проигрывание плэйлиста

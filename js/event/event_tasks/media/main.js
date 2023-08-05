2a5ca5bfa3ccaaf988ac8b8f86e03938dd3b8788
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-08-05 14:58:09 +0300

    Добавляет время проигрывания аудиофайла

--- js/event/event_tasks/media/main.js
+++ js/event/event_tasks/media/main.js
@@ -11,40 +11,49 @@ window.addEventListener('DOMContentLoaded', function(){
   // ждем когда прогрузиться аудио
   audio.addEventListener('loadedmetadata', function(){
 
-      // получене кнопок и программирование их
-      const btnPlay = document.querySelector('.btn-play'),
-        btnPause = document.querySelector('.btn-pause');
+    // получене кнопок и программирование их
+    const btnPlay = document.querySelector('.btn-play'),
+          btnPause = document.querySelector('.btn-pause');
+  
+    btnPlay.addEventListener('click', function () {
+      this.classList.add('hide');
+      btnPause.classList.remove('hide');
+      audio.play();
+    });
 
-      btnPlay.addEventListener('click', function () {
-        this.classList.add('hide');
-        btnPause.classList.remove('hide');
-        audio.play();
-      })
-
-      btnPause.addEventListener('click', function () {
-        this.classList.add('hide');
-        btnPlay.classList.remove('hide');
-        audio.pause();
-      });
+    btnPause.addEventListener('click', function () {
+      this.classList.add('hide');
+      btnPlay.classList.remove('hide');
+      audio.pause();
+    });
     
     // расчет общего времени и вывод 
-    const timeOutput = document.querySelector('.full-time')
+    const timeOutput = document.querySelector('.full-time'),
+          timeStartOutput = document.querySelector('.time-start');
     let time = audio.duration/60;
     // вывод времени в формате мин:сек 4.23
     function modFormatTime(currentTime){
       currentTime = currentTime.toFixed(2);
       let min = Math.floor(currentTime);
-      let sec = Math.floor((currentTime - min)*100);  
+      let sec = Math.floor((currentTime - min)*100);
       return {min: min,
               sec: sec}    
     }
     let fullTime = modFormatTime(time);
     timeOutput.textContent = `${fullTime.min}:${fullTime.sec}`;
 
-    // timeOutput.textContent = time.toFixed(2);
-  })
-
+    // расчет и вывод времени проигрывания
+    // добавление события обновления времени
+    audio.addEventListener('timeupdate', function(){
+      let pastTime = audio.currentTime,
+          minutes = Math.floor((pastTime % 3600)/60),
+          seconds = Math.floor(pastTime % 60),
+          timeOutput = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`; 
+          timeStartOutput.textContent = timeOutput;
+    })
+  });
+  // 
+});
   // бесконечное проигрывание плэйлиста
   // регулировка громкости
   // плейлист
-});

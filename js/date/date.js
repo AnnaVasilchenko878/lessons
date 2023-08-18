d1adde13a3309ef1d9b539d4b8f6c3d6bf4c397f
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-08-18 13:00:52 +0300

    Добавляет описание получения даты и времени с примерами

--- js/date/date.js
+++ js/date/date.js
@@ -6,3 +6,14 @@
 // объект с датой
 // let date = new Date();
 // console.log(date);
+// Fri Aug 18 2023 10:37:11 GMT+0300 (Москва, стандартное время)
+
+// let date = new Date(10154854564135);
+// console.log(date);
+// Sun Oct 18 2291 03:56:04 GMT+0300 (Москва, стандартное время)
+
+// let date = new Date('1995-01-20');
+// console.log(date);
+
+let date = new Date('1985-02-10T15:35:20');
+console.log(date);

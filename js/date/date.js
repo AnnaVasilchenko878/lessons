f59f38aa6cc2aa4302252cf23bffbca60bf9e3a6
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-08-18 10:57:21 +0300

    Добавляет тему дата и время

---
+++ js/date/date.js
@@ -0,0 +1,8 @@
+// строка с датой
+// let date = Date();
+// console.log(typeof date);
+// Fri Aug 18 2023 10:37:11 GMT+0300 (Москва, стандартное время)
+
+// объект с датой
+// let date = new Date();
+// console.log(date);
